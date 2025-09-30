55ce80f0fb88a8c2c7a61b9ca30c553847563868
Melissa Pascal <melissa@pascalsbakery.com>
2025-09-30 09:51:23 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,8 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
